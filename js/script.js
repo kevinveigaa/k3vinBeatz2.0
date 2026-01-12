fbd79275057bcae61cc47755f1f26fc58cdb5531
kevinveigaa <92695662+kevinveigaa@users.noreply.github.com>
2026-01-12 16:23:42 -0300

    Atualizar o script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.log("K3VIN BEATZ carregado 🎧🔥");
-
 document.addEventListener('DOMContentLoaded', () => {
   // TILT 3D
   document.querySelectorAll('.beat-card').forEach(card => {
@@ -23,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
     menuDropdown.style.display = menuDropdown.style.display === 'block' ? 'none' : 'block';
   });
 
-  // FILTRO POR CATEGORIA
   document.querySelectorAll('.category-btn').forEach(btn => {
     btn.addEventListener('click', () => {
       const category = btn.getAttribute('data-category');
@@ -34,22 +31,29 @@ document.addEventListener('DOMContentLoaded', () => {
         const back = document.createElement('button');
         back.textContent = 'Voltar para todos os beats';
         back.id = 'backBtn';
-        back.style.margin = '20px';
+        back.style.margin = '20px auto';
         back.style.padding = '10px 20px';
         back.style.cursor = 'pointer';
         back.onclick = () => location.reload();
         document.querySelector('.lista-beats').prepend(back);
       }
-    });
-  });.
-
-  // BUSCA POR NOME
-  const searchInput = document.getElementById('searchInput');
-  searchInput.addEventListener('input', () => {
-    const value = searchInput.value.toLowerCase();
-    document.querySelectorAll('.beat-card').forEach(card => {
-      const name = card.dataset.name.toLowerCase();
-      card.style.display = name.includes(value) ? 'flex' : 'none';
+      menuDropdown.style.display = 'none';
     });
   });
+
+  // PESQUISA INTELIGENTE
+  const searchInput = document.getElementById('searchInput');
+  const searchBtn = document.getElementById('searchBtn');
+  function normalizeString(str){
+    return str.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g,"");
+  }
+  function searchBeats(){
+    const value = normalizeString(searchInput.value);
+    document.querySelectorAll('.beat-card').forEach(card => {
+      const name = normalizeString(card.dataset.name);
+      card.style.display = name.includes(value) ? 'flex' : 'none';
+    });
+  }
+  searchInput.addEventListener('input', searchBeats);
+  searchBtn.addEventListener('click', searchBeats);
 });
